72abf3c58d13c3e52f1ee56712615946809acb4f
Okoronkwo Alfred Chinedu <okoronkwoac@yahoo.com>
2020-09-10 16:22:26 -0400

    completed implementation

--- Controllers/PatientsController.cs
+++ Controllers/PatientsController.cs
@@ -4,23 +4,25 @@ using Microsoft.AspNetCore.Mvc;
 using TestWebApi.Data;
 using TestWebApi.Data.Entities;
 using TestWebApi.Dtos;
+using TestWebApi.Services;
 
 namespace TestWebApi.Controllers
 {
     public class PatientsController : BaseAPIController
     {
-        private readonly IGenericRepository<Patient> _genericRepo;
+        private readonly PatientsService _patientsService;
+
         private readonly IMapper _mapper;
-        public PatientsController(IGenericRepository<Patient> genericRepo, IMapper mapper)
+        public PatientsController(PatientsService patientsService, IMapper mapper)
         {
+            _patientsService = patientsService;
             _mapper = mapper;
-            _genericRepo = genericRepo;
         }
 
         [HttpGet]
         public ActionResult<IList<PatientDto>> GetPatients()
         {
-            var list = _genericRepo.ListAll().Result;
+            var list = _patientsService.ListAll().Result;
             var result = _mapper.Map<IList<Patient>, IList<PatientDto>>(list);
             return Ok(result);
         }
@@ -28,7 +30,7 @@ namespace TestWebApi.Controllers
         [HttpGet("{id}")]
         public ActionResult<PatientDto> GetPatientById(int id)
         {
-            var patient = _genericRepo.GetById(id).Result;
+            var patient = _patientsService.GetById(id).Result;
             var result = _mapper.Map<Patient, PatientDto>(patient);
             return Ok(result);
         }
@@ -37,7 +39,7 @@ namespace TestWebApi.Controllers
         public ActionResult<Patient> CreatePatient(PatientDto patientDto)
         {
             var patient = _mapper.Map<PatientDto, Patient>(patientDto);
-            var result = _genericRepo.Create(patient).Result;
+            var result = _patientsService.Create(patient).Result;
             return Ok(result);
         }
 
@@ -46,18 +48,26 @@ namespace TestWebApi.Controllers
         {
             var patient = _mapper.Map<PatientDto, Patient>(patientDto);
 
-            var existingPatient = _genericRepo.GetById(patient.Id).Result;
-            patient.Appointments = existingPatient.Appointments;
-            
-            var result = _genericRepo.Update(patient).Result;
+            /*var existingPatient = _patientsService.GetById(patient.Id).Result;
+            patient.Appointments = existingPatient.Appointments;*/
+
+            var result = _patientsService.Update(patient).Result;
             return Ok(result);
         }
 
-        [HttpDelete]
-        public ActionResult<bool> DeletePatient(Patient Patient)
+        [HttpDelete("{id}")]
+        public ActionResult<bool> DeletePatient(int id)
         {
-            var result = _genericRepo.Delete(Patient).Result;
-            return Ok(result);
+            var patient = _patientsService.GetById(id).Result;
+            if (patient != null)
+            {
+                var result = _patientsService.Delete(patient).Result;
+                if (result) {
+                    return Ok(result);
+                } 
+                return BadRequest("Unable to delete record");               
+            }
+            return BadRequest("No matching record was found");
         }
     }
 }
